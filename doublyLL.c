97731d84a6b23f2a4fd23c42b3ada61fcfa9c020
salazarSD <satyam9mzp@gmail.com>
2023-02-03 15:05:12 +0530

    LEetS see its working or not\

--- doublyLL.c
+++ doublyLL.c
@@ -1,1 +1,20 @@
 #include<stdio.h>
+#include<stdlib.h>
+
+struct node {
+    struct node * prev;
+    int data;
+    struct node * next;
+};
+int main(){
+    struct node *head=NULL;
+    struct node *ptr;
+    head =creatList(head);
+    //travelsing the list:
+    ptr=head;
+    while(ptr!=NULL){
+        printf("%d ",ptr->data);
+        ptr=ptr->next;
+    }
+    return 0;
+}
