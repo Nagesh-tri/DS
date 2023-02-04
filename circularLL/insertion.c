9a071bbf1ccbdcb1383bfdf1eca82d4fd58009ea
salazarSD <satyam9mzp@gmail.com>
2023-02-04 11:46:57 +0530

    insertion in between nodes done!

--- circularLL/insertion.c
+++ circularLL/insertion.c
@@ -20,13 +20,64 @@ struct node* addToEmpty(int data){
     temp->next= temp;
     return temp;
 }
-
+//
 struct node* addTobeg(struct node* tail,int data){
     struct node* newP= malloc(sizeof(struct node));
     newP->data =data;
     newP->next =tail->next;
     tail->next =newP;
     return tail;
+}/*
+insertion at end:
+we just need to
+step 1:update next part of newNode with add of 1st node
+step 2:after that update next part of tail node with add of newNode 
+step 3: update tail with newNode add thatas ittt
+*/
+struct node* addAtEnd(struct node* tail, int data){
+    //creating new node
+    struct node* newP = malloc(sizeof(struct node));
+    newP->data = data;
+    newP->next = NULL;
+    //step1:
+    newP->next= tail->next;
+    //step2:
+    tail->next= newP;
+    //step3:
+    tail=newP; //also can do tail=tail->next
+    return tail;
+}/*
+Insertion between the nodes:
+target to add newNode afer postion 2;
+step1: we need to traverse till that postion
+step 2: update next part of newNOde with add of next node at particular position
+step3: update next of particular node after that newNode to be inserted with add of newNOde
+step4: we also have to check wheter user wants to add node maybe after last node, in that case 
+we also have to update tail pointer with add of newNode
+*/
+struct node* addAfterPos(struct node* tail,int data,int pos){
+    //new node
+    struct node* newP = malloc(sizeof(struct node));
+    newP->data = data;
+    newP->next = NULL;
+    //step1
+    struct node* p = tail->next;
+    while (pos>1)
+    {
+        p=  p->next;
+        pos--;
+    }
+    //step2
+    newP->next=p->next;
+    //step 3
+    p->next=newP;
+    //step 4
+    if (p==tail)
+    {
+        tail=newP;
+    }
+    return tail;
+    
 }
 struct node* printcll(struct node* tail){
     struct node* p = tail->next;
@@ -40,6 +91,10 @@ void main(){
     struct node* tail;
     tail =addToEmpty(45);
     tail =addTobeg(tail,595);
+    //add at end
+
+    tail=addAtEnd(tail,76);
+    tail=addAtEnd(tail,67);
 
     printcll(tail);
 }
---
+++ circularLL/traverse.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/*
+
+we know that we have 1st node add in tail->next
+in order to traverse we start from 1st node, by using a pointer 'p', but the thing is in cll(circular LL)
+last node is not contains the 'null' so how to determine when to stop traversing
+we also know that p(starts pointing from 1st node) reached the 1st node again we should stop
+to do so we use 'do while' with condition p contails the same add as inn tail pointer i.e (p != tail->next)
+*/
+struct node* printcll(struct node* tail){
+    struct node* p = tail->next;
+    do{
+        printf("%d ", p->data);
+        p=p->next;
+    }while(p != tail->next);
+}
