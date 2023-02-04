839cef9897c0543361746c144026f770efacfcba
salazarSD <satyam9mzp@gmail.com>
2023-02-04 14:13:26 +0530

    deletion function end

--- circularLL/deletion.c
+++ circularLL/deletion.c
@@ -1,3 +1,4 @@
+#include<stdlib.h>
 /*
 deleting first node:
 step1: take a temp pointer pointing to 1st node
@@ -7,6 +8,12 @@ step3: free temp and put null in temp
 
 Time complexity: O(1)
  */
+struct node
+{
+    int data;
+    struct node* next;
+};
+
 struct node* delfirst(struct node* tail){
     //1
     struct node* temp;
@@ -16,4 +23,40 @@ struct node* delfirst(struct node* tail){
     //3
     free(temp);
     temp= NULL;
+}
+
+/*
+Delete last node:
+step 1: tavelsel to the 2nd last node of the list as we don't have any pointer to point 
+that node which we need coz it will be the last node after deltion
+step2: update 2nd last node's next part with 1st node add (present in tail)
+step3: free tail
+step4: update tail with temp
+Time Complexity: O(n)
+*/
+struct node* dellast(struct node* tail){
+    //check if no node present
+    if(tail==NULL)
+        return tail;
+    //if only one node is present
+    if (tail->next==tail)
+    {
+        free(tail);
+        tail=NULL;
+        return tail;
+    }
+    
+    struct node* temp =tail->next;//1st node
+    //1
+    while (temp->next !=tail)
+    {
+        temp=temp->next;
+    }
+    //2
+    temp->next=tail->next;
+    //3
+    free(tail);
+    //4
+    tail=temp;
+    return tail;
 }
